81ee381bc5f1537f7774eb928ecd7d83e9d05890
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2024-12-19 11:00:52 +0100

    feat: include api doc and test

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import dogRoutes from "./routes/dogRoutes";
 import catRoutes from "./routes/catRoutes";
 import bunnyRoutes from "./routes/bunnyRoutes";
 import docRoutes from "./routes/docRoute";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3001;
@@ -15,6 +21,9 @@ app.use("/api", dogRoutes);
 app.use("/api", catRoutes);
 app.use("/api", bunnyRoutes);
 app.use("/", docRoutes);
+app.get("/api", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "api-docs.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
